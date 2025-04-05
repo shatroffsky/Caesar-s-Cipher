62c07cb480ab1b06197c166c6a9f03802661d1e3
Vladyslav Shatrovskyi <shatroffsky@gmail.com>
2025-04-05 19:24:14 +0300

    Конструктори для різних випадків запуску.

--- src/CaesarCipher/CaesarCipher.java
+++ src/CaesarCipher/CaesarCipher.java
@@ -1,7 +1,18 @@
 package CaesarCipher;
 
-public class CaesarCipher {
+import static CaesarCipher.ConstantsForAlphabet.UK;
+import static CaesarCipher.ConstantsForAlphabet.ENG;
 
+public class CaesarCipher {
+    private final int key;
+
+    public CaesarCipher(int key) {
+        this.key = key;
+    }
+
+    public CaesarCipher() {
+        this.key = 0;
+    }
 }
 
 

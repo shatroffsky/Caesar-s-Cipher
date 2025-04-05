923bf9089eb51d90f8bf3c39eff1876dd7288e60
Vladyslav Shatrovskyi <shatroffsky@gmail.com>
2025-04-05 19:44:51 +0300

    Готовий GUI через консоль.

--- src/RunProgram/GuiInConsole.java
+++ src/RunProgram/GuiInConsole.java
@@ -8,16 +8,14 @@ import java.util.Scanner;
 
 public class GuiInConsole {
 
-    private char option;
-
     public GuiInConsole() {
         Menu();
     }
 
     public GuiInConsole(String[] args){
         switch (args[0]) {
-//            case "ENCRYPT" -> MenuIfArgsENCRYPT(args);
-//            case "DECRYPT" -> MenuIfArgsDECRYPT(args);
+            case "ENCRYPT" -> MenuIfArgsENCRYPT(args);
+            case "DECRYPT" -> MenuIfArgsDECRYPT(args);
             case "BRUTE_FORCE" -> new BrudeForce(Path.of(args[1]));
             default -> System.out.println("Not found command.");
         }
@@ -27,15 +25,75 @@ public class GuiInConsole {
         System.out.println("Hello! It's Caesar's cipher program.");
         boolean run = true;
         while (run) {
-//            option = Option();
+            char option = Option();
             if (option == 'e') {
                 run = false;
             } else if (option == 'a') {
-//                run = OptionA();
+                run = OptionA();
             } else if (option == 'b') {
-//                run = OptionB();
+                run = OptionB();
             } else {
                 System.out.println("Sorry, you write wrong option, try again.");
+            }
+        }
+    }
+
+    public boolean Exit(){
+        System.out.println("Do you want to continue? | [a]Yes | [b]No |");
+        return new Scanner(System.in).next().charAt(0) == 'a';
+    }
+
+    public boolean OptionA(){
+        System.out.println("Enter your path to file with text which you need to encrypt: ");
+        Path path = Path.of(new Scanner(System.in).nextLine());
+        System.out.println("Enter your key to encrypt: ");
+        int key = new Scanner(System.in).nextInt();
+        FileToEncrypt file = new FileToEncrypt(path, key);
+        if (file.GetAlphabet().equals("UK")) {
+            file.EncrytpFileUA();
+        } else if (file.GetAlphabet().equals("ENG")) {
+            file.EncrytpFileENG();
+        }
+        return Exit();
+    }
+
+    public boolean OptionB(){
+        System.out.println("Enter your path to file with text which you need to decrypt: ");
+        Path path = Path.of(new Scanner(System.in).nextLine());
+        System.out.println("Enter your key to decrypt: ");
+        int key = new Scanner(System.in).nextInt();
+        FileToDecrypt file = new FileToDecrypt(path, key);
+        if (file.GetAlphabet().equals("UK")) {
+            file.DecrytpFileUA();
+        } else if (file.GetAlphabet().equals("ENG")) {
+            file.DecrytpFileENG();
+        }
+        return Exit();
+    }
+
+    public char Option(){
+        System.out.println("Choose your option:\n[a] Encrypt text | [b] Decrypt text | [e] Exit program");
+        return new Scanner(System.in).next().charAt(0);
+    }
+
+    public void MenuIfArgsENCRYPT(String[] args){
+        if (args[0].equals("ENCRYPT")){
+            FileToEncrypt file = new FileToEncrypt(Path.of(args[1]), Integer.parseInt(args[2]));
+            if (file.GetAlphabet().equals("UK")) {
+                file.EncrytpFileUA();
+            } else if (file.GetAlphabet().equals("ENG")) {
+                file.EncrytpFileENG();
+            }
+        }
+    }
+
+    public void MenuIfArgsDECRYPT(String[] args){
+        if (args[0].equals("DECRYPT")){
+            FileToDecrypt file = new FileToDecrypt(Path.of(args[1]), Integer.parseInt(args[2]));
+            if (file.GetAlphabet().equals("UK")) {
+                file.DecrytpFileUA();
+            } else if (file.GetAlphabet().equals("ENG")) {
+                file.DecrytpFileENG();
             }
         }
     }

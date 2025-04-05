658afb3e8771714f2dea63d9baec1aaaefd53fda
Vladyslav Shatrovskyi <shatroffsky@gmail.com>
2025-04-05 19:56:33 +0300

    Протестований Main та запуск через аргументи.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import RunProgram.Runner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        new Runner(args);
+
     }
 }

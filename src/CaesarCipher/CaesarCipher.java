3dbba10369811b7c02807795946dd806450d402d
Vladyslav Shatrovskyi <shatroffsky@gmail.com>
2025-04-05 19:25:20 +0300

    Логіка Шифрування/Розшифрування різних мов.

--- src/CaesarCipher/CaesarCipher.java
+++ src/CaesarCipher/CaesarCipher.java
@@ -13,6 +13,55 @@ public class CaesarCipher {
     public CaesarCipher() {
         this.key = 0;
     }
+
+    public String EncryptUA(String StringToEncrypt) {
+        StringBuilder result = new StringBuilder();
+        for (char el : StringToEncrypt.toCharArray()){
+            if (inUK(el)){
+                result.append(UK.get((UK.indexOf(el) + (key % UK.size())) % UK.size()));
+            } else {
+                result.append(el);
+            }
+        }
+        return result.toString();
+    }
+
+    public String DecryptUA(String StringToDecrypt) {
+        StringBuilder result = new StringBuilder();
+        for (char el : StringToDecrypt.toCharArray()){
+            if (inUK(el)){
+                result.append(UK.get((UK.indexOf(el) - (key % UK.size()) + UK.size()) % UK.size()));
+            } else {
+                result.append(el);
+            }
+        }
+        return result.toString();
+    }
+
+    public String EncryptENG(String StringToEncrypt) {
+        StringBuilder result = new StringBuilder();
+        for (char el : StringToEncrypt.toCharArray()){
+            if (inENG(el)){
+                result.append(ENG.get((ENG.indexOf(el) + (key % ENG.size())) % ENG.size()));
+            } else {
+                result.append(el);
+            }
+        }
+        return result.toString();
+    }
+
+    public String DecryptENG(String StringToDecrypt) {
+        StringBuilder result = new StringBuilder();
+        for (char el : StringToDecrypt.toCharArray()){
+            if (inENG(el)){
+                result.append(ENG.get((ENG.indexOf(el) - (key % ENG.size()) + ENG.size()) % ENG.size()));
+            } else {
+                result.append(el);
+            }
+        }
+        return result.toString();
+    }
+
 }
 
 
